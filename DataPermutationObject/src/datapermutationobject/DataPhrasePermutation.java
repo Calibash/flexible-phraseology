500f4ede113117beb179b68cd0aa443ba4f105e4
Calibash <wanderingvagrant@hotmail.com>
2016-12-29 00:43:33 -0600

    Initial class commit for Data Phrase Permutation

---
+++ DataPermutationObject/src/datapermutationobject/DataPhrasePermutation.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datapermutationobject;
+
+/**
+ *Context for data permutation object structure
+ *
+ * 
+ * 
+ * @author Calibash
+ */
+public class DataPhrasePermutation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
