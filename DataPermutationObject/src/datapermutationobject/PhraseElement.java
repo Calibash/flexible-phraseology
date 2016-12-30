99088c061f19813b53a4915464638a0bd80ec3bf
Calibash <wanderingvagrant@hotmail.com>
2016-12-30 00:54:20 -0600

    Initial commit of PhraseElement interface with initial description

---
+++ DataPermutationObject/src/datapermutationobject/PhraseElement.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datapermutationobject;
+
+/**
+ *Phrase element object/interface (PEO/PEI); i am tentatively considering this 
+ * as a conversational or elemental component which fits the needs of a 
+ * particular task or objective. the interface should define the purpose of the 
+ * phrase, while the concrete implementations should form individually duck-type
+ * compatible components which ought to be usable interchangeably - if they are 
+ * not, then the phrase element interface isn't in scope, or the object itself 
+ * isn't fully in the correct scope.
+ * 
+ * @author Calibash
+ */
+public interface PhraseElement {
+    
+}
