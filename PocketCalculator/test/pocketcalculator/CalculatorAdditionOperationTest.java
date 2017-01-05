944e746a22dc015de44e98376404bf2376f0ae53
Calibash <wanderingvagrant@hotmail.com>
2017-01-05 00:03:24 -0600

    added initial tests for calculator addition

---
+++ PocketCalculator/test/pocketcalculator/CalculatorAdditionOperationTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pocketcalculator;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Calibash
+ */
+public class CalculatorAdditionOperationTest {
+    
+    public CalculatorAdditionOperationTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of performOperation method, of class CalculatorAdditionOperation.
+     */
+    @Test
+    public void testPerformSmallAdditionOperation() {
+        System.out.println("performOperation");
+//        final DependencyToMockClass mock
+        ICalcValue input1 = new CalcBaseValue(10);
+        ICalcValue input2 = new CalcBaseValue(14);
+        CalculatorAdditionOperation instance = new CalculatorAdditionOperation();
+        double expResult = 24;
+        ICalcValue result = instance.performOperation(input1, input2);
+        assertEquals(expResult, result.presentValue(), 0);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+    
+}
