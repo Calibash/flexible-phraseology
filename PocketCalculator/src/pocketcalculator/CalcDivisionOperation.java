b05142b78467aef0f328298daa86d94746751952
Calibash <wanderingvagrant@hotmail.com>
2017-01-06 15:26:54 -0600

    Added division operation, basic testing, division by zero basic support

---
+++ PocketCalculator/src/pocketcalculator/CalcDivisionOperation.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pocketcalculator;
+
+/**
+ *
+ * @author Calibash
+ */
+public class CalcDivisionOperation implements ICalcOperation{
+    public CalcDivisionOperation(){}
+    @Override
+    public ICalcValue performOperation(
+            ICalcValue firstValue, 
+            ICalcValue secondValue) {
+        if (secondValue.presentValue()!= 0){            
+            return new CalcBaseValue(
+                firstValue.presentValue() / secondValue.presentValue());
+        }
+        throw new UnsupportedOperationException(
+                    "Division by zero, Not supported yet.");
+  }
+    
+}
---
+++ PocketCalculator/test/pocketcalculator/CalcDivisionOperationTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pocketcalculator;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Calibash
+ */
+public class CalcDivisionOperationTest {
+    
+    public CalcDivisionOperationTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of performOperation method, of class CalcDivisionOperation.
+     */
+    @Test
+    public void testPerformOperation() {
+        System.out.println("performOperation");
+        ICalcValue firstValue = new CalcBaseValue(320);
+        ICalcValue secondValue = new CalcBaseValue(10);
+        CalcDivisionOperation instance = new CalcDivisionOperation();
+        ICalcValue expResult = new CalcBaseValue(32);
+        ICalcValue result = instance.performOperation(firstValue, secondValue);
+        assertEquals(expResult.presentValue(), result.presentValue(), 0);
+    }
+    
+    @Test
+        public void testDividesByZeroDoesntCrashEverything() {
+        System.out.println("DividesByZeroDoesntCrashEverything");
+        ICalcValue firstValue = new CalcBaseValue(320);
+        ICalcValue secondValue = new CalcBaseValue(0);
+        CalcDivisionOperation instance = new CalcDivisionOperation();
+        Boolean exceptionThrown = false;
+        try{
+            instance.performOperation(firstValue, secondValue);
+        } catch (UnsupportedOperationException e){
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+//        assertEquals(expResult.presentValue(), result.presentValue(), 0);
+    }
+}
