d38655d779c54905fc4a02e73c54847926fc1457
Calibash <wanderingvagrant@hotmail.com>
2017-01-17 23:44:29 -0600

    added small double subtraction test

--- PocketCalculator/test/pocketcalculator/CalcSubtractionOperationTest.java
+++ PocketCalculator/test/pocketcalculator/CalcSubtractionOperationTest.java
@@ -42,6 +42,16 @@ public class CalcSubtractionOperationTest {
         assertEquals(expResult.presentValue(), result.presentValue(), 0);
     }
     
+    @Test void testPerformSmallDoubleSubtraction(){
+        System.out.println("Small double subtraction");
+        ICalcValue firstValue = new CalcValue(734.234);
+        ICalcValue secondValue = new CalcValue(2553.23);
+        CalcSubtractionOperation instance = new CalcSubtractionOperation();
+        ICalcValue expResult = new CalcValue(-1818.966);
+        ICalcValue result = instance.performOperation(firstValue, secondValue);
+        assertEquals(expResult.presentValue(), result.presentValue(), 0);
+    }
+    
     @Test
     public void testPerformSubtractionOnNegativeNumbers(){
         System.out.println("subtracting a negative from a negative number");
