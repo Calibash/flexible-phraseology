2f7e80e12ac75b6dedb3d880af38fd623867db82
Calibash <wanderingvagrant@hotmail.com>
2017-01-17 23:36:36 -0600

    added test for small double multiplication

--- PocketCalculator/test/pocketcalculator/CalcMultiplicationOperationTest.java
+++ PocketCalculator/test/pocketcalculator/CalcMultiplicationOperationTest.java
@@ -42,4 +42,15 @@ public class CalcMultiplicationOperationTest {
         assertEquals(expResult.presentValue(), result.presentValue(),0);
     }
     
+    @Test
+    public void testPerformOperationSmallDoubleMultiplication(){
+        System.out.println("small integer multiplication");
+        ICalcValue firstValue = new CalcValue(10.23);
+        ICalcValue secondValue = new CalcValue(25.45);
+        CalcMultiplicationOperation instance = new CalcMultiplicationOperation();
+        ICalcValue expResult = new CalcValue(260.3535);
+        ICalcValue result = instance.performOperation(firstValue, secondValue);
+        assertEquals(expResult.presentValue(), result.presentValue(),0);  
+    }
+    
 }
