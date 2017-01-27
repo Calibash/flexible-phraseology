bf26609f2eb288e63641fb7b53e0ebe260a8abe4
Calibash <wanderingvagrant@hotmail.com>
2017-01-26 18:41:18 -0600

    tentative outline of calc equation functionality

---
+++ PocketCalculator/src/pocketcalculator/CalcEquation.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pocketcalculator;
+
+/**
+ * A queue of inputted values and operations, to be output to the calculator 
+ * display on command
+ * @author Calibash
+ */
+public class CalcEquation {
+    //take input of some series of values and operations. new values go into the
+    //stored queue, to be displayed on the screen until cleared
+    
+    //output the current value of the existing equation
+    
+    //delete the most recent operation or value
+    
+    //add a new value or operation to the equation   
+    
+}
