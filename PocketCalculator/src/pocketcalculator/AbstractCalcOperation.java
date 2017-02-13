31a742609091379c8cf7124eb750a7b65d05f7b9
Calibash <wanderingvagrant@hotmail.com>
2017-02-12 23:22:50 -0600

    clarified documentation on abstract calc operation

--- PocketCalculator/src/pocketcalculator/AbstractCalcOperation.java
+++ PocketCalculator/src/pocketcalculator/AbstractCalcOperation.java
@@ -6,11 +6,19 @@
 package pocketcalculator;
 
 /**
- *
+ *Abstract of passable calculation operations. encapsulates logic specific to 
+ * what operation is being performed, and how for use in pocket calculator
  * @author Calibash
  */
 public abstract class AbstractCalcOperation implements ICalcOperation{
-
+    
+    /**
+     * Abstract version of operation. Performs implemented operation on the two
+     * inputs
+     * @param firstValue
+     * @param secondValue
+     * @return 
+     */
     @Override
     public ICalcValue performOperation(ICalcValue firstValue, ICalcValue secondValue) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
