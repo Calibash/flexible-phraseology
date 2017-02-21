a0882f1dbc4a2f8f6d8b27f06dc82802a38fc603
Calibash <wanderingvagrant@hotmail.com>
2017-02-20 22:10:23 -0600

    added CalcRunner class to combine elements into GUI output dock

---
+++ PocketCalculator/src/pocketcalculator/CalcRunner.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pocketcalculator;
+
+/**
+ * this class exists because i was trying to do too much abstract shit too soon, 
+ * and went enough different directions that I sort of lost track what i was 
+ * initially doing, which was to create a running calculator. 
+ * 
+ * purpose; serve as a stopping point for GUI/mechanical operation
+ * 
+ * present; functioning interfaces needed to run GUI in whatever form GUI needs
+ * (ie as a form of adaptor) 
+ * 
+ * utilize; existing and future interfaces to subcomponents
+ * @author Calibash
+ */
+public class CalcRunner {
+    public CalcRunner(){
+    
+    }
+    
+}
