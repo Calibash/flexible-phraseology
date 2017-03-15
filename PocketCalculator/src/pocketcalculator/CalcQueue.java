127a60b2747819ed7510b4f1bddd4db95260a222
Calibash <wanderingvagrant@hotmail.com>
2017-03-14 23:57:27 -0500

    concrete queue planning notes added

--- PocketCalculator/src/pocketcalculator/CalcQueue.java
+++ PocketCalculator/src/pocketcalculator/CalcQueue.java
@@ -6,11 +6,21 @@
 package pocketcalculator;
 
 /**
- *
+ *A module specific implementation or facade for queue operations, specifically 
+ * using module classes.
+ * 
+ * -should be able to handle calcValues.
+ * -the function of the queue will be to (as of right now) input a series of
+ * value/operation pairs, either short or long, and then execute them when the 
+ * equals key is pushed. so something like 2*5+4 should be possible.
+ * -the question is how this queue will use the order of operations? stopgap 
+ * measure would be to not. one operation at a time, make use of functionality
+ * but forgo any immediate messing about with that.
  * @author Calibash
  */
 public class CalcQueue extends AbstractCalcQueue {
-    
+    //either implement own, or use java library
+    //potential options interface, collection, list
     public CalcQueue(AbstractCalcValue defaultValue) {
         super(defaultValue);
     }
