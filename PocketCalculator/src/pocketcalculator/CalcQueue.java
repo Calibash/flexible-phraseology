2dd81b12ae5676380231d17afa396e06e1989ee4
Calibash <wanderingvagrant@hotmail.com>
2017-03-14 23:48:35 -0500

    added concrete implementation for calc queue

---
+++ PocketCalculator/src/pocketcalculator/CalcQueue.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pocketcalculator;
+
+/**
+ *
+ * @author Calibash
+ */
+public class CalcQueue extends AbstractCalcQueue {
+    
+    public CalcQueue(AbstractCalcValue defaultValue) {
+        super(defaultValue);
+    }
+    
+}
