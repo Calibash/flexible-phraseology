abc1a64c23a2a032bb1b2510abd5b1c6528f6e1e
Calibash <wanderingvagrant@hotmail.com>
2017-04-19 23:45:12 -0500

    de-enabling calcrunner operations. logical operation suspected to be wonky, and should have had test coverage before check-in.

--- PocketCalculator/src/pocketcalculator/CalcRunner.java
+++ PocketCalculator/src/pocketcalculator/CalcRunner.java
@@ -31,23 +31,28 @@ public class CalcRunner {
     public void enterValue(AbstractCalcValue value){
         //add value to queue
         previousEntries.addToQueue(this.currentOperation, value);
+         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     public void callMultiplication(){
         //add a new multiplication operation instance onto the queue
         this.currentOperation = new CalcMultiplicationOperation();
+         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     public void callDivision(){
         //add a new division operation instance onto the queue
         this.currentOperation = new CalcDivisionOperation();
+         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     public void callAddition(){
         //add a new addition operation instance onto the queue
         this.currentOperation = new CalcAdditionOperation();
+         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     public void callSubtraction(){
         //add a new subtraction operation instance onto the queue
         this.currentOperation = new CalcSubtractionOperation();
+         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     public AbstractCalcValue callEquals(){
         //output updated display value
